79dc07326edd9ba8a4c913ad027241fe4d818d19
davidjanke <davidjanke@gmail.com>
2015-09-26 17:13:49 +0200

    Implement topN and topology D

--- src/TopNFinderBolt.java
+++ src/TopNFinderBolt.java
@@ -5,57 +5,125 @@ import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
-import java.util.HashMap;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * a bolt that finds the top n words.
  */
 public class TopNFinderBolt extends BaseBasicBolt {
-  private HashMap<String, Integer> currentTopWords = new HashMap<String, Integer>();
-  private int N;
+    private ConcurrentHashMap<String, Integer> currentTopWords = new ConcurrentHashMap<String, Integer>();
 
-  private long intervalToReport = 20;
-  private long lastReportTime = System.currentTimeMillis();
+    private int N;
 
-  public TopNFinderBolt(int N) {
-    this.N = N;
-  }
+    private final long intervalToReport = 20;
+    private long lastReportTime = System.currentTimeMillis();
 
-  @Override
-  public void execute(Tuple tuple, BasicOutputCollector collector) {
- /*
-    ----------------------TODO-----------------------
-    Task: keep track of the top N words
-
-
-    ------------------------------------------------- */
-
-
-    //reports the top N words periodically
-    if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
-      collector.emit(new Values(printMap()));
-      lastReportTime = System.currentTimeMillis();
+    public TopNFinderBolt(int N) {
+        this.N = N;
     }
-  }
 
-  @Override
-  public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        /*
+        ----------------------TODO-----------------------
+        Task: keep track of the top N words
 
-     declarer.declare(new Fields("top-N"));
+        ------------------------------------------------- */
 
-  }
+        currentTopWords.put(tuple.getString(0), tuple.getInteger(1));
+        cleanToTopNWords();
 
-  public String printMap() {
-    StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append("top-words = [ ");
-    for (String word : currentTopWords.keySet()) {
-      stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+        //reports the top N words periodically
+        if (System.currentTimeMillis() - lastReportTime >= intervalToReport) {
+            collector.emit(new Values(printMap()));
+            lastReportTime = System.currentTimeMillis();
+        }
     }
-    int lastCommaIndex = stringBuilder.lastIndexOf(",");
-    stringBuilder.deleteCharAt(lastCommaIndex + 1);
-    stringBuilder.deleteCharAt(lastCommaIndex);
-    stringBuilder.append("]");
-    return stringBuilder.toString();
 
-  }
+    private void cleanToTopNWords() {
+        TreeSet<Integer> sortedCounts = new TopXTreeSet<Integer>(N);
+        sortedCounts.addAll(currentTopWords.values());
+
+        int minimum = sortedCounts.first();
+
+        for(String word : currentTopWords.keySet()) {
+            if(currentTopWords.get(word) < minimum)
+            currentTopWords.remove(word);
+        }
+    }
+
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        declarer.declare(new Fields("top-N"));
+
+    }
+
+/*
+    private static class TopNValueHashMap<K, V extends Comparable<V>> extends HashMap<K, V> {
+        private final TopXTreeSet<V> valueTopSet;
+        private final HashMap<K,V> backingHashMap = new HashMap<K, V>();
+        private final int limit;
+
+        public TopNValueHashMap(Integer entryLimit) {
+            this.limit = entryLimit;
+            valueTopSet = new TopXTreeSet<V>(entryLimit);
+        }
+
+        @Override
+        public V put(K key, V value) {
+            checkIfToBeAdded(value);
+            return super.put(key, value);
+        }
+
+        private boolean checkIfToBeAdded(K key, V value) {
+            if (valueTopSet.size() < limit) {
+                return true;
+            } else {
+                return (valueTopSet.first().compareTo(value) < 0);
+            }
+        }
+
+
+    }
+*/
+    private static class TopXTreeSet<V> extends TreeSet<V> {
+        private final int limit;
+
+        public TopXTreeSet(int limit) {
+            this.limit = limit;
+        }
+
+        @Override
+        public boolean add(V e) {
+            boolean addSuccess = super.add(e);
+            if(addSuccess) {
+                reduceToLimit();
+            }
+            return addSuccess;
+        }
+
+        private void reduceToLimit() {
+            if(size() > limit) {
+                remove(first());
+            }
+        }
+    }
+
+    public String printMap() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("top-words = [ ");
+        for (String word : currentTopWords.keySet()) {
+            stringBuilder.append("(" + word + " , " + currentTopWords.get(word) + ") , ");
+        }
+        int lastCommaIndex = stringBuilder.lastIndexOf(",");
+        stringBuilder.deleteCharAt(lastCommaIndex + 1);
+        stringBuilder.deleteCharAt(lastCommaIndex);
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+
+    }
+
 }
--- src/TopWordFinderTopologyPartD.java
+++ src/TopWordFinderTopologyPartD.java
@@ -2,9 +2,7 @@
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.*;
 import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
@@ -15,42 +13,60 @@ import backtype.storm.tuple.Values;
  */
 public class TopWordFinderTopologyPartD {
 
-  private static final int N = 10;
+    private static final int N = 10;
 
-  public static void main(String[] args) throws Exception {
+    static final String countBoltName = "count";
+    static final String spoutName = "spout";
+    static final String splitBoltName = "split";
+    static final String normalizeBoltName = "normalize";
+    static final String topNBoltName = "top-n";
 
-
-    TopologyBuilder builder = new TopologyBuilder();
-
-    Config config = new Config();
-    config.setDebug(true);
-
-
-    /*
-    ----------------------TODO-----------------------
-    Task: wire up the topology
-
-    NOTE:make sure when connecting components together, using the functions setBolt(name,…) and setSpout(name,…),
-    you use the following names for each component:
-    
-    FileReaderSpout -> "spout"
-    SplitSentenceBolt -> "split"
-    WordCountBolt -> "count"
-	NormalizerBolt -> "normalize"
-    TopNFinderBolt -> "top-n"
+    public static void main(String[] args) throws Exception {
 
 
-    ------------------------------------------------- */
+        TopologyBuilder builder = new TopologyBuilder();
+
+        Config config = new Config();
+        config.setDebug(true);
 
 
-    config.setMaxTaskParallelism(3);
+        /*
+        ----------------------TODO-----------------------
+        Task: wire up the topology
 
-    LocalCluster cluster = new LocalCluster();
-    cluster.submitTopology("word-count", config, builder.createTopology());
+        NOTE:make sure when connecting components together, using the functions setBolt(name,…) and setSpout(name,…),
+        you use the following names for each component:
 
-    //wait for 2 minutes and then kill the job
-    Thread.sleep(2 * 60 * 1000);
+        FileReaderSpout -> "spout"
+        SplitSentenceBolt -> "split"
+        WordCountBolt -> "count"
+        NormalizerBolt -> "normalize"
+        TopNFinderBolt -> "top-n"
 
-    cluster.shutdown();
-  }
+        ------------------------------------------------- */
+
+        config.put("inputFileName", args[0]);
+
+        IRichSpout spout = new FileReaderSpout(args[0]);
+        IBasicBolt splitSentenceBolt = new SplitSentenceBolt();
+        IBasicBolt wordCountBolt = new WordCountBolt();
+        IBasicBolt normalizerBolt = new NormalizerBolt();
+        IBasicBolt topNFinderBolt = new TopNFinderBolt(N);
+
+        builder.setSpout(spoutName, spout, 1);
+        builder.setBolt(splitBoltName, splitSentenceBolt, 8).shuffleGrouping(spoutName);
+        builder.setBolt(normalizeBoltName, normalizerBolt, 8).shuffleGrouping(splitBoltName);
+        builder.setBolt(countBoltName, wordCountBolt).fieldsGrouping(normalizeBoltName, new Fields("word"));
+        builder.setBolt(topNBoltName, topNFinderBolt, 1).allGrouping(countBoltName);
+
+        config.setMaxTaskParallelism(3);
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("word-count", config, builder.createTopology());
+
+        //wait for 2 minutes and then kill the job
+        Thread.sleep(2 * 60 * 1000);
+
+        cluster.shutdown();
+    }
 }
